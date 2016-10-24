ae8d73f9185473b50681c3e8d5144e16a0aa2e6c
satorusasozaki <sasozakisatoru@gmail.com>
2016-10-24 02:26:48 -0700

    Wire up all the outlets

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,50 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Satoru Sasozaki on 10/24/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var dollarSignLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        // whenever business is changed
+        didSet {
+            thumbImageView.setImageWith(business.imageURL!)
+            nameLabel.text = business.name
+            distanceLabel.text = business.distance
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbImageView.layer.cornerRadius = 3
+        // http://stackoverflow.com/questions/20449256/how-does-clipstobounds-work
+        thumbImageView.clipsToBounds = true
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var businesses: [Business]!
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.tableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -41,19 +45,19 @@ class BusinessesViewController: UIViewController {
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let businesses = businesses {
+            return businesses.count
+        }
+        return 0
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        cell.business = businesses[indexPath.row]
+        return cell
+        
+    }
     
 }
